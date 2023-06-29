41d5b6795205d32423b3e1828c24ac26d30fca3d
B-C-Punith <bcpunith46@gmail.com>
2023-06-29 13:20:03 +0530

    msg

---
+++ june28th/ForestRun.java
@@ -0,0 +1,29 @@
+class Forest
+{
+    Forest()
+    {
+        System.out.println("Forest is the main resourse of th enation");
+    }
+}
+
+class ForestRun
+{
+    public static void main(String [] args)
+    {
+        Forest amazon = new Forest();
+        Forest nilagiri = new Forest();
+        Forest hagare = new Forest();
+        Forest nile = new Forest();
+        Forest abharanay = new Forest();
+        Forest fufa = new Forest();
+        Forest bandipur = new Forest();
+        Forest  safari= new Forest();
+        Forest ocasia = new Forest();
+
+        Forest westrangharts = new Forest();
+        Forest annamali = new Forest();
+        
+
+
+    }
+}
---
+++ june28th/TreeRun.java
@@ -0,0 +1,27 @@
+class Tree
+{
+Tree()
+    {
+        System.out.println("The tree give the oxygen to the atmoshher");
+    }
+}
+
+class TreeRun
+{
+    public static void main(String [] args)
+    {
+        Tree nilagiri = new Tree();
+        Tree coconut = new Tree();
+        Tree mango = new Tree();
+        Tree ocasia = new Tree();
+        Tree silver = new Tree();
+        Tree pongaminapinnata = new Tree();
+        Tree jackfruit = new Tree();
+        Tree guva = new Tree();
+        Tree apple = new Tree();
+        Tree banana = new Tree();
+        Tree pomagronet = new Tree();
+    
+
+    }
+}
